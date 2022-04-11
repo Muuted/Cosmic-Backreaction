d6a2042b9071e20c75f59396f728d4804dd19902
Muuted <adam.s.knudsen@gmail.com>
2022-04-11 19:44:06 +0200

    trying stuff

--- numerical/solving LTB model initial.py
+++ numerical/solving LTB model initial.py
@@ -39,6 +39,14 @@ def rho(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb):
     rrho = (1 / (4 * np.pi * G)) * dMMdr / (RR ** 2 * dRRdr)
     return rrho
 
+def ddRdrdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb):
+    sqrts = np.sqrt(
+        2*MM/RR +2*EE + Lamb*RR**2/3
+    )
+
+    extra = 2*dMMdr/RR - 4*MM*dRRdr/RR**2 +2*dEEdr + 2*Lamb*RR*dRRdr/3
+
+    return extra/sqrts
 
 def dSdt(S, t, G, rho_i, r_b, n, m, A, H, Lamb):
     r, RR, EE, MM, dMMdr, dRRdr, dEEdr = S
@@ -51,11 +59,14 @@ def dSdt(S, t, G, rho_i, r_b, n, m, A, H, Lamb):
     sol_rho = rho(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
     return [r, sol_EE, sol_MM, sol_dEEdr, sol_dMMdr, sol_dRdt, sol_rho]
 
-def dSdt2(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb):
-    sol_EE = E(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
-    sol_MM = M(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
-    sol_dRRdt = dRdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+'''
+def dSdt2(S, r, G, rho_i, r_b, n, m, A, H, Lamb):
+    RR, EE, MM, = S
 
+    sol_dEEdr = dEdr(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+    sol_dMMdr = dMdr(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+    
+'''
 
 # values of the constants in the eq's
 Lamb = 0
@@ -64,8 +75,8 @@ r_b = 5e10
 n = 2
 m = 2
 H = 1
-G = 6.67e-11 # Nm**2/kg**2 = m**3/kg*s**2
-rho_i = 1 #1e-26 #kg/m^3
+G = 6.67e-11  # Nm**2/kg**2 = m**3/kg*s**2
+rho_i = 1  # 1e-26 #kg/m^3
 args_list = (G, rho_i, r_b, n, m, A, H, Lamb)
 
 # Initial value conditions
@@ -73,8 +84,8 @@ r_init = 1
 R_init = 1
 E_init = 1
 M_init = 1
-dMdr_init=  1
-dRdr_init= 1
+dMdr_init = 1
+dRdr_init = 1
 dEdr_init = 1
 init_condt = [r_init, R_init, E_init, M_init, dMdr_init, dRdr_init, dEdr_init]
 
@@ -93,14 +104,13 @@ print(ans.shape, ans.shape[0], ans.shape[1])
 print(ans)
 
 plt.figure()
-plt.plot(ans[0],label='r')
-plt.plot(ans[1],label='E(r)')
-plt.plot(ans[2],label='M(r)')
-plt.plot(ans[3],label='dEdr')
-plt.plot(ans[4],label='dMdr')
-plt.plot(ans[5],label='dRdt')
-plt.plot(ans[6],label='rho')
+plt.plot(ans[0], label='r')
+plt.plot(ans[1], label='E(r)')
+plt.plot(ans[2], label='M(r)')
+plt.plot(ans[3], label='dEdr')
+plt.plot(ans[4], label='dMdr')
+plt.plot(ans[5], label='dRdt')
+plt.plot(ans[6], label='rho')
 plt.legend()
-
 
 plt.show()
