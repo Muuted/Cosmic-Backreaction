80a7e6adf19f0e182698c57928bf783ae97d772b
Muuted <adam.s.knudsen@gmail.com>
2022-05-02 16:42:33 +0200

    simple try

--- numerical/LBT simpleton version.py
+++ numerical/LBT simpleton version.py
@@ -1,4 +1,3 @@
-from re import S
 import scipy.integrate
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
@@ -49,11 +48,22 @@ def ddRdrdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
 
     return extra/sqrts
 
+def dSdt_dRdrdt(S,t,p):
+    dRdr = S
+    r,dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb = p
 
-'''
-Making a for loop on the radius, where we will integrate with the ODEint; on time;
-for each loop
-'''
+    ans_dRdrdt = ddRdrdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+
+    return ans_dRdrdt
+
+def dSdt_dRdt(S,t,p):
+    R = S
+    r,dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb = p
+
+    ans_dRdt = dRdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+
+    return ans_dRdt
+
 
 # values of the constants in the eq's
 Lamb = 0
@@ -66,12 +76,43 @@ G = 6.67e-11  # Nm**2/kg**2 = m**3/kg*s**2
 rho_i = 1  # 1e-26 #kg/m^3
 args_list = (G, rho_i, r_b, n, m, A, H, Lamb)
 
-# Initial value conditions
-r_init = 1
-R_init = 1
-E_init = 1
-M_init = 1
-dMdr_init = 1
-dRdr_init = 1
-dEdr_init = 1
-init_condt = [r_init, R_init, E_init, M_init, dMdr_init, dRdr_init, dEdr_init]
+'''
+Making a for loop on the radius, where we will integrate with the ODEint; on time;
+for each loop
+We have to have an initial R, which I will for now just set to zero
+'''
+RR = 1
+dRRdr = 1
+MM = 1
+EE = 1
+dMMdr = 1
+dRRdr = 1
+dEEdr = 1
+for r in range(99,100):
+    '''
+    The initial conditions are found for each r, and used in the ODE int integration
+    '''
+    ans = []
+    EE = E(r, RR,EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+    MM = M(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+    dEEdr = dEdr(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+    dMMdr= dMdr(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb)
+    args_list =[r,dMMdr, dRRdr, dEEdr, G, rho_i, r_b, n, m, A, H, Lamb]
+    #args_list = [r, dMMdr, dEEdr, G, rho_i, r_b, n, m, A,H,Lamb]
+    init_cond = [0]
+    time_tot = np.linspace(0, 10, 10)
+    #ans = scipy.integrate.odeint(dSdt_for_dr,y0=init_cond,args=args_list)
+    ans = scipy.integrate.odeint(dSdt_dRdt,
+        t=time_tot,
+        y0=init_cond,
+        args=(args_list,)
+        )
+    print(r)
+
+#ans = ans.T
+print('ans shape = ',ans.shape)
+print('time shape =',time_tot.shape)
+plt.figure()
+plt.plot(time_tot,ans)
+plt.show()
+
