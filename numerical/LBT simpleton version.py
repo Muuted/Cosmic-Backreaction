e200bb8812648785b65f3714aa7f73203b7047a0
Muuted <adam.s.knudsen@gmail.com>
2022-05-05 12:31:10 +0200

    simpler plotting

--- numerical/LBT simpleton version.py
+++ numerical/LBT simpleton version.py
@@ -39,7 +39,8 @@ G = 6.67e-11  #  m^3/kg/s^2
 G = 6.67e-11*one_solar_mass*one_Gy**2/(one_Mpc**3) # Mpc^3/M_o*Gy^2
 
 rho_FLRW =8.7e27 # kg/m^3 # This is the critical density Ryden in the back
-rho_FLRW = 1.5e-7 #M_o/Mpc^3 # 8.7e27*one_Mpc**3/one_solar_mass # M_o/Mpc^3
+rho_FLRW = 8.7e27*one_Mpc**3/one_solar_mass # M_o/Mpc^3
+rho_FLRW = 1.5e-7
 # The unit for rho_FLRW is fond on the website:
 # http://astroweb.case.edu/ssm/astroconstants.html
 # right above the Galactic untis
@@ -92,53 +93,41 @@ for i in range(0,num_interations):
     init_cond_dRdt = [a_i*r, a_i]
     
     r += dr
-    
-    if ivp == False:
-        ans = scipy.integrate.odeint(dSdt_dRdrdt,t=time_tot,y0=init_cond_dRdt,
-            args=(args_list_ODE,)
-            )
-    if ivp == True:
-        ans = solve_ivp(dSdt_dRdrdt_ivp,t_span=(t_start,t_end),
-                        y0=init_cond_dRdt ,args=args_for_ivp,
-                        method ='LSODA'
-                        #,t_eval = time_tot
-                        )
         
 
-if ivp == False:
-    ans = ans.T
-    R_vec = ans[0]
-    print('len(R)=',len(R_vec))
-    dRdr_vec = ans[1]
-    print('len(dRdr)=',len(dRdr_vec))
-    print(R_vec)
-    print('R_max /R_min=',max(R_vec)/min(R_vec), '\n',
-        'dRdr_max/dRdr_min =',max(dRdr_vec)/min(dRdr_vec))
-    print('H=',H, '\n',
-        'G=',G,'\n',
-        'rho_FLRW =',rho_FLRW
+    ans_odeint = scipy.integrate.odeint(dSdt_dRdrdt,t=time_tot,y0=init_cond_dRdt,
+        args=(args_list_ODE,)
         )
-    plt.figure()
-    plt.plot(time_tot.T,R_vec,'-',label='R(t,r)')
-    plt.legend()
 
-    plt.figure()
-    plt.plot(time_tot.T,dRdr_vec,'-',label=r'$\frac{\partial R}{\partial r}$')
-    plt.legend()
-    
+    ans_ivp = solve_ivp(dSdt_dRdrdt_ivp,t_span=(t_start,t_end),
+                    y0=init_cond_dRdt ,args=args_for_ivp,
+                    method ='LSODA'
+                    #,t_eval = time_tot
+                    )
 
-if ivp == True:
 
-    plt.figure()
 
-    plt.plot(ans.y[0],'r',label='R')
-    plt.legend()
+ans_odeint = ans_odeint.T
+R_vec = ans_odeint[0]
+print('len(R)=',len(R_vec))
+dRdr_vec = ans_odeint[1]
+print('len(dRdr)=',len(dRdr_vec))
+print(R_vec)
+print('R_max /R_min=',max(R_vec)/min(R_vec), '\n',
+    'dRdr_max/dRdr_min =',max(dRdr_vec)/min(dRdr_vec))
+print('H=',H, '\n',
+    'G=',G,'\n',
+    'rho_FLRW =',rho_FLRW
+    )
+plt.figure()
+plt.plot(time_tot.T,R_vec,label=r'$R(t,r)_{odeint}$')
+plt.plot(ans_ivp.t,ans_ivp.y[0],'--',label=r'$R(t,r)_{ivp}')
+plt.legend()
 
-    plt.figure()
-    plt.plot(ans.y[1],'r',label=r'$\frac{\partial R}{\partial r}$')
-
-    plt.legend()
-
+plt.figure()
+plt.plot(time_tot.T,dRdr_vec,label=r'$\frac{\partial R}{\partial r}_{odeint}$')
+plt.plot(ans_ivp.t,ans_ivp.y[1],'--',label=r'$\frac{\partial R}{\partial r}_{ivp}$')
+plt.legend()
 
 
 plt.show()
