2d4bcc86f25c678575ea59548ecdf1b0a641816f
Muuted <adam.s.knudsen@gmail.com>
2022-05-08 20:58:20 +0200

    de sitter

--- numerical/Einstein_de_sitter_functions.py
+++ numerical/Einstein_de_sitter_functions.py
@@ -9,6 +9,7 @@ from scipy.integrate import solve_ivp
 
 import numpy as np
 
+#from constants import *
 
 # units for conversion
 one_Mpc = 3.086e22 # m
@@ -17,3 +18,20 @@ one_Gy = 3.156e16/60*60*24*365 # years
 one_solar_mass = 1.989e30 #kg
 
 
+
+def func_a_Ein_Sitter(a,G):
+
+    const = 8*np.pi*G/3
+    dadt = np.sqrt(
+        const/a
+    )
+
+    return dadt
+
+def func_rho_Ein_Sitter(a):
+
+    const = 1
+
+    drhodt = const/a**3
+
+    return drhodt
