a89848bae894932d1464c22a7ac065210aba27cb
Muuted <adam.s.knudsen@gmail.com>
2022-05-16 09:24:44 +0200

    sdf

--- numerical/LBT simpleton version.py
+++ numerical/LBT simpleton version.py
@@ -73,34 +73,40 @@ ans_a_ES = rho_ES
 plt.figure()
 plt.subplot(2,3,1)
 plt.plot(time_tot,ans_RR/r, label='R(t,r)/r')
+plt.xlabel('Gyr')
 #plt.title('R/r')
 plt.legend()
 
 plt.subplot(2,3,2)
 plt.plot(time_vec,a_ES,label='a(t) EdS')
+plt.xlabel('Gyr')
 plt.legend()
 #plt.title('a(t) Ein de Sitter')
 
 plt.subplot(2,3,3)
 plt.plot(time_tot,ans_RR/r,label='R(t,r)/r')
 plt.plot(time_vec,a_ES,'--',label='a(t) EdS')
+plt.xlabel('Gyr')
 #plt.title('R vs a EdS')
 plt.legend()
 
 plt.subplot(2,3,4)
 plt.plot(time_tot,ans_dRdr,label='dRdr')
+plt.xlabel('Gyr')
 plt.legend()
 #plt.title('dRdr')
 
 
 plt.subplot(2,3,5)
-plt.plot(time_tot,ans_M,label='Mass')
+plt.plot(time_tot,ans_M,label='M(r)')
+plt.xlabel('Gyr')
 plt.legend()
 #plt.title('M')
 
 
 plt.subplot(2,3,6)
-plt.plot(time_tot,ans_rho,label='mass density')
+plt.plot(time_tot,ans_rho,label=r'$\rho$(t,r)')
+plt.xlabel('Gyr')
 plt.legend()
 #plt.title('rho')
 
