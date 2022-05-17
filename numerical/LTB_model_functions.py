bb9008f87b95c536844c1ab51254167c582d6f5f
Muuted <adam.s.knudsen@gmail.com>
2022-05-17 13:30:38 +0200

    sdfsdf

--- numerical/LTB_model_functions.py
+++ numerical/LTB_model_functions.py
@@ -10,24 +10,7 @@ from scipy.integrate import solve_ivp
 import numpy as np
 from The_constants import *
 
-'''
-r = List[0]
-    RR = List[1]
-    EE = List[2]
-    MM = List[3]
-    dMMdr = List[4]
-    dRRdr = List[5]
-    dEEdr = List[6]
-    G = List[7]
-    rho_FLRW = List[8]
-    r_b = List[9]
-    n = List[10]
-    m = List[11]
-    A = List[12]
-    H = List[13]
-    Lamb = List[14]
-    c = List[15]
-'''
+
 Lamb, A, r_b, n, m, H_0, H_i, G, rho_c0, rho_i0, a_i, t_i, t_0,c= func_constants()
 
 def func_E(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_c0, r_b, n, m, A, H, Lamb,c):
@@ -58,7 +41,7 @@ def func_dEdr(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_c0, r_b, n, m, A, H, La
 
 def func_rho(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_c0, r_b, n, m, A, H, Lamb,c):
 
-    kappa = (8*np.pi)#*rho_c0
+    kappa = (8*np.pi)*rho_c0
 
     rho = (2/kappa)*(dMMdr/(RR**2*dRRdr))
 
@@ -119,87 +102,6 @@ def func_LTB_dSdt(S,t,p):
 
     return_list = [ dRdt, dRdrdt, rho ]#, ana_MM]
     return  return_list
-
-
-"""
-def M(List):
-    
-    RR = List[1]
-    EE = List[2]
-
-    rho_FLRW = List[8]
-    H = List[13]
-    
-    MM =( 4 * np.pi * RR ** 3 * rho_FLRW / 3) * (1 - 2 * EE / (5 * H ** 2 * RR ** 2))
-
-    return MM
-
-def dMdr(List):
-    
-    RR = List[1]
-    EE = List[2]
-    dRRdr = List[5]
-    dEEdr = List[6]
-    rho_FLRW = List[8]
-    H = List[13]
-
-    dMMdr1 = 12 * np.pi * RR ** 2 * dRRdr * rho_FLRW / 3
-
-    dMMdr2 = -(8 * np.pi * rho_FLRW / 15 * H ** 2) * (EE * dRRdr + RR * dEEdr)
-
-    return dMMdr1 + dMMdr2
-
-def func_rho(List):
-    
-    RR = List[1]
-    dMMdr = List[4]
-    dRRdr = List[5]
-    G = List[7]
-
-    rrho = (1 / (4 * np.pi * G)) * dMMdr / (RR ** 2 * dRRdr)
-
-
-    return rrho  
-
-def dSdt_dRdrdt(S,t,p):
-    R, dRdr = S
-    r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb = p
-    
-    ans_dRdr = ddRdrdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb)
-    ans_R = dRdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb)
-    
-    return [ans_R, ans_dRdr]
-
-def dSdt_dRdrdt_ivp(t,S,r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb):
-    R, dRdr = S
-    #r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb = p
-    
-    ans_dRdr = ddRdrdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb)
-    ans_R = dRdt(r, RR, EE, MM, dMMdr, dRRdr, dEEdr, G, rho_FLRW, r_b, n, m, A, H, Lamb)
-    
-    return [ans_R, ans_dRdr]
-
-
- """
-# For the de Sitter model universe, for comparison with, our model of the universe. 
-
-def func_FLRW_R_dRdr(t_vec,r_func,H_0):
-
-    # R = a(t)*r
-    # a(t) = (t/t_0)^(3/2)
-    t_0 = 2/(3*H_0)
-
-    List = [[],[]]
-
-    for i in range(0,len(t_vec)):
-
-        a_FLRW = (t_vec[i]/t_0)**(3/2)
-        R_FLRW = a_FLRW*r_func
-
-        List[0].append(R_FLRW)
-        List[1].append(a_FLRW)
-
-    return List
 
 
 
