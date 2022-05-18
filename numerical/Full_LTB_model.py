d802f265bb0e3928ae5eabf7a42642340f3160a3
Muuted <adam.s.knudsen@gmail.com>
2022-05-18 12:54:40 +0200

    Made another document for the integration over r as well

--- numerical/Full_LTB_model.py
+++ numerical/Full_LTB_model.py
@@ -6,11 +6,13 @@ import plotly.graph_objects as go
 
 Lamb, A, r_b, n, m, H_0, H_i, G, rho_c0, rho_i0, a_i, t_i, t_0,c= func_constants()
 
-num_steps = 10000 # number of steps between t_start and t_end
-num_interations = 1 #number of r's
 dt = 1e3        # time step
-r = r_b*5         # distance
-dr = 300        # change of r
+dr = 1        # change of r
+r = r_b*5          # distance, we start at origo 
+num_steps = 10000 # number of steps between t_start and t_end
+num_interations = 1#int(r_b/dr) #number of r's
+
+print(num_interations)
 
 # Initial condition for the functions
 RR = r*a_i
@@ -29,34 +31,53 @@ args_list =[r, EE, dEEdr,MM, dMMdr, G, rho_c0, r_b, n, m, A, H_0, Lamb,c]
 # Our time vector for the integration
 time_tot = np.linspace(t_i,t_0,num_steps)
 
+# Creating the lists for the data at different r values
+ans_RR = [[] for x in range(num_interations)]
+ans_dRdr = [[] for x in range(num_interations)]
+ans_rho = [[] for x in range(num_interations)]
 
-#The initial conditions are found for each r, and used in the ODE int integration
-init_cond_dRdt = [RR, dRRdr]#, rho_c0]#, MM ]
+ans_dMMdr = []
 
-# ----------------------------Integration start ------------------------------------------------------------
+for i in range(0,num_interations): # This loop makes it so that we iterate over r
+    
+    #The initial conditions are found for each r, and used in the ODE int integration
+    init_cond_dRdt = [RR, dRRdr, rho_c0]
+    args_list =[r, EE, dEEdr,MM, dMMdr, G, rho_c0, r_b, n, m, A, H_0, Lamb,c]
 
-ans_odeint = scipy.integrate.odeint(func_LTB_dSdt, 
-        y0=init_cond_dRdt, 
-        t=time_tot,
-        args=(args_list,)
-        )
-# The results from our odeint  above
-ans_odeint = ans_odeint.T
+    #print(init_cond_dRdt)
+    ans_odeint = scipy.integrate.odeint(func_LTB_dSdt, 
+            y0=init_cond_dRdt, 
+            t=time_tot,
+            args=(args_list,)
+            )
+    # The results from our odeint  above
+    ans_odeint = ans_odeint.T
+    
+    ans_RR[i] = ans_odeint[0]
+    ans_dRdr[i] = ans_odeint[1]
+    ans_rho[i] = ans_odeint[2]
 
-# --------------------------Integration done ------------------------------------------------------------
+    r += dr
+    # Now we have the part that changes from the range of r 
 
-ans_RR = ans_odeint[0]
-ans_dRdr = ans_odeint[1]
-ans_rho = ans_odeint[2]
+    """EE = func_E(r, r_b, n, m, A)
+    dEEdr = func_dEdr(r, r_b, n, m, A)
+    MM = func_M(r, EE, G, rho_c0, a_i, H_i, c)
+    dMMdr = func_dMMdr(r, EE, dEEdr, rho_c0, H_i, a_i, c)
+    rho_c0 = func_rho(r, RR, dMMdr, dRRdr, rho_c0) """
+    
+    # conditions for integration ---- 
+    
 
-
+print(type(ans_RR))
+#print(ans_rho[0])
 # Results for the Einstein de Sitter model 
 a_ES, rho, rho_ES, time_vec = Einstein_de_sitter(num_of_steps=num_steps)
 ans_a_ES = rho_ES
 
 plt.figure()
 plt.subplot(2,3,1)
-plt.plot(time_tot,ans_RR/r, label=r'$\dfrac{R(t,r)}{r}$')
+plt.plot(time_tot,ans_RR[0]/r, label=r'$\dfrac{R(t,r)}{r}$')
 plt.xlabel('Gyr')
 #plt.title('R/r')
 plt.legend()
@@ -69,14 +90,14 @@ plt.legend()
 #plt.title('a(t) Ein de Sitter')
 
 plt.subplot(2,3,3)
-plt.plot(time_tot,ans_RR/r,label=r'$\dfrac{R(t,r)}{r}$')
+plt.plot(time_tot,ans_RR[0].T/r,label=r'$\dfrac{R(t,r)}{r}$')
 plt.plot(time_vec,a_ES,'--',label=r'$a_{EdS}$')
 plt.xlabel('Gyr')
 #plt.title('R vs a EdS')
 plt.legend()
 
 plt.subplot(2,3,4)
-plt.plot(time_tot,ans_dRdr,label=r'$\dfrac{\partial R(t,r)}{\partial r}$')
+plt.plot(time_tot,ans_dRdr[0],label=r'$\dfrac{\partial R(t,r)}{\partial r}$')
 plt.xlabel('Gyr')
 plt.legend()
 #plt.title('dRdr')
