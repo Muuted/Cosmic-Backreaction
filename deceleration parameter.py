ffe05564b96b0a1dbe035526e113852c785661f2
LeaK99 <103244277+LeaK99@users.noreply.github.com>
2022-05-28 07:56:07 +0200

    Update deceleration parameter.py

--- deceleration parameter.py
+++ deceleration parameter.py
@@ -22,36 +22,48 @@ def q_2(theta):
     return (1 - np.cos(theta)) / np.sin(theta) ** 2
 
 
-#f_2 = 0.1
+f_2 = []
 T = []
-Q = []
+Q =[[],[],[],[],[],[],[]]
+V2 =[[],[],[],[],[],[],[]]
+numV=7
 Q_2 = []
 Q_1 = []
 V_2=[]
 V_1=[]
+colorcode=['b','g','r','c','m','y','b','g','r','c','m','y','b','g','r','c','m','y']
 plt.figure()
-for f_2 in np.arange(0.2, 0.8,0.1):
+for i in range(numV):
+    f_2.append(round(0.1*(1+i),1))
     for theta in np.arange(0.00001, 5, 0.001):
-        V = v(theta, f_2)
+        V = v(theta, f_2[i])
         H = h(theta)
         Q2 = q_2(theta)
-        Q.append(q(V, H, Q2))
-        Q_2.append(Q2)
-        Q_1.append(0)
+        Q[i].append(q(V, H, Q2))
+        #Q_2.append(Q2)
+        #Q_1.append(0)
         V_2.append(V)
         V_1.append(1-V)
-        T.append(theta)
-    plt.plot(T, Q, color='b', label=r'$q(\theta)$, $v_t=$'+f'{f_2}')
+        V2[i].append(V)
+        if i==0:
+            T.append(theta)
+i=0     
+Qq=np.transpose(Q) 
+for i in range(numV):
+    plt.plot(T, V2[i], label=r'$v(\theta)$ for $v_t=$'+f'{f_2[i]}')
+    #plt.plot(T, Q[i], label=r'$q(\theta)$ for $v_t=$'+f'{f_2[i]}')  
+plt.legend()  
 #plt.plot(T, Q_2, 'b',label=r'$q_2(\theta)$')
 #plt.plot(T, Q_1, 'g',label=r'$q_1(\theta)$')
 #plt.plot(T, V_1, 'g',label='Volume fraction in region 1')
 #plt.plot(T, V_2, 'b',label='Volume fraction in region 2')
-
+#plt.plot(T, Q, label=r'$q(\theta)$, $v_t=$'+f'{f_2}')
+#plt.plot(T, Q)
 plt.xlim(0, 5)
-plt.axhline(y=0, color='r', linestyle='--')
+#plt.axhline(y=0, color='k', linestyle='--')
 plt.xlabel(r'$\theta$')
-#plt.ylabel(r'v($\theta$)')
+plt.ylabel(r'v($\theta$)')
 #plt.ylabel(r'q($\theta$)')
-plt.legend()
-#plt.title(r'Volume fraction v as a'+'\n'+r'function of development angle $\theta$')
+
+plt.title(r'Volume fraction $v$ as a'+'\n'+r'function of development angle $\theta$')
 plt.show()
